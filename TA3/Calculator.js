f29522825daab4e7080b2d9ced242cbba40e004f
RainMazino <154502781+RainMazino@users.noreply.github.com>
2025-03-18 17:22:53 +0800

    Create Calculator.js

---
+++ TA3/Calculator.js
@@ -0,0 +1,17 @@
+function appendCharacter(char) {
+    document.getElementById("display").value += char;
+}
+function clearDisplay() {
+    document.getElementById("display").value = "";
+}
+function deleteLast() {
+    let display = document.getElementById("display");
+    display.value = display.value.slice(0, -1);
+}
+function calculate() {
+    try {
+        document.getElementById("display").value = eval(document.getElementById("display").value);
+    } catch (e) {
+        document.getElementById("display").value = "Error";
+    }
+}
